b293b1d04cf5d57832710a69d6376ec0e373dcf7
Salman Mohammed <salman.MSH@outlook.com>
2024-05-03 11:46:48 -0400

    Latest code

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
 using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http;
@@ -24,12 +25,13 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
-       
+
         public IActionResult FetchDataAsync()
         {
             Console.WriteLine("fetch data called");
             return Ok();
         }
+
         [HttpPost]
         public ActionResult UpdateOrder()
         {
@@ -38,25 +40,30 @@ namespace WebApplication2.Controllers
             return Json(new { success = true, message = "Order updated successfully" });
         }
 
-        [HttpPost]
-        public async Task<IActionResult> FetchDataAsync1([FromBody] string startTime)
+        public class TimeModel
         {
-            Console.WriteLine("fetch data called");
+            public string Time { get; set; }
+        }
+        [HttpPost]
+        public async Task<IActionResult> FetchDataAsync1([FromBody] TimeModel Time)
+        {
+            Console.WriteLine("fetch data called", Time.Time);
             try
             {
                 // Convert the start time to Unix timestamp
-                DateTime parsedStartTime = DateTime.Parse(startTime);
-                //DateTime parsedStartTime = new DateTime(2024, 5, 2, 15, 44, 22, 216);
-                long unixStartTime = (long)(parsedStartTime.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+                DateTime parsedStartTime = DateTime.Parse(Time.Time);
+                long unixStartTime = (long)(parsedStartTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc))).TotalSeconds;
+
+                //long unixStartTime = 1714731200;
 
 
                 var subreddit = "movies";
-                var apiUrl = $"https://oauth.reddit.com/r/{subreddit}/new?after={unixStartTime}";
-
+                var apiUrl = $"https://oauth.reddit.com/r/{subreddit}/new?limit=100";
+                var accessToken = "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzE0ODM1Mzc2Ljc4MDA2LCJpYXQiOjE3MTQ3NDg5NzYuNzgwMDYsImp0aSI6ImJRUXc5RGdwUmt6alNoRGg2ME1GWUpzVUJXS0FmZyIsImNpZCI6ImN1LW5qU2ZReTRkU1MxYUs0ZXFhRFEiLCJsaWQiOiJ0Ml9nd3ZsY3hqZDkiLCJhaWQiOiJ0Ml9nd3ZsY3hqZDkiLCJsY2EiOjE2OTExODI3MjkxMjcsInNjcCI6ImVKeUtWdEpTaWdVRUFBRF9fd056QVNjIiwiZmxvIjo5fQ.BpgMwkRbhsKnWZWSf7cy4zcnxiGozOmCogboGm8a98fASx0RIDic-NG0jfkMeRoeG18J50NnkMMaGzM3o3l6tx0n9RZvinppTppmrIUATV1ioPxUaxK1tvC7d6h-Br8jaJQuT0AvjhPTycKZ16IdQgpnbhXJodVFpGfI9YF4Yj3OUly25rF8xWGVUdT87qktkdQ6W-AC8OB_QO2o2HHnhQu2BUXQJw76oVuXA3jbZUuwoKX7QECpiTulJWyi7RnQa2P70m4zHI8KntTbiBHyS6ZjbcQpu9oKu3kTfALMYS5RrOgm_3Vj7DNYSugTpkn_U_pFzMlf9XIJAPfN15FQbw";
                 using (var client = _httpClientFactory.CreateClient())
                 {
                     // Add the necessary headers for authorization and user agent
-                    client.DefaultRequestHeaders.Add("Authorization", $"bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzE0NzU1MzY0LjEzODQ2MywiaWF0IjoxNzE0NjY4OTY0LjEzODQ2MywianRpIjoiVWxadFBaT05OczNGU2ZDVnpGbU5kTHM5Tm9nWE9nIiwiY2lkIjoiY3UtbmpTZlF5NGRTUzFhSzRlcWFEUSIsImxpZCI6InQyX2d3dmxjeGpkOSIsImFpZCI6InQyX2d3dmxjeGpkOSIsImxjYSI6MTY5MTE4MjcyOTEyNywic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.JIDNLomUTH5NFrkLZ8AWjLfDk78MSiTbxB6irxZ5SJDwx9ht_yWSZvP6w7Wr6qT_Vk0OyfAtprUuUeZdhoKW2Wv_4VtlF4GEk1LXx7BR9TLpNJ-oP2GQvfvwbOZdrDoqE7rC4elCohjeikSKj0sPBZW624MQ57a3Ib5u4LF6S0_uIDTv5jRHV2lRG9lvRu9oYRQCoOGOolRDB94711e7COk5DBL2q2UnsGSAnbRSP2-mTUTOFe2qKxVA0UDScbiXZc-rDOmncEr89KCPw7bdcRvGda3kLBJ7eJ1VxSxYYSk5GRo7yt1_khGBqaUtaD28klLY6C_m7nk5vX2eDMd5tw");
+                    client.DefaultRequestHeaders.Add("Authorization", $"bearer {accessToken}");
                     client.DefaultRequestHeaders.Add("User-Agent", "ChangeMeClient/0.1 by YourUsername");
 
                     // Make the GET request to the Reddit API
@@ -71,34 +78,70 @@ namespace WebApplication2.Controllers
                         // Deserialize the JSON response into a dynamic object
                         dynamic jsonData = JsonConvert.DeserializeObject(redditData);
 
-                        // Extract the required data
-                        var mostUpvotedPost = ((IEnumerable<dynamic>)jsonData.data.children)
+                        List<dynamic> filteredPosts = new List<dynamic>();
+
+                        // Iterate through the posts and filter based on creation time
+                        foreach (var post in jsonData.data.children)
+                        {
+                            // Convert the creation time of the post to Unix timestamp
+                            long postUnixTime = (long)post.data.created_utc;
+
+                            // Check if the post was created after the specified start time
+                            if (postUnixTime > unixStartTime)
+                            {
+                                // Add the post to the filtered list
+                                filteredPosts.Add(post);
+                            }
+                        }
+
+                        if (filteredPosts.Count == 0)
+                        {
+                            return Ok(new
+                            {
+                                MostUpvotedPostUrl = "No post after the start time",
+                                MostUpvotedPostUpvotes = 0,
+                                MostActiveUser = "No users posted",
+                                MostActiveUserPostCount = 0
+                            });
+                        }
+
+
+                        // Extract the most upvoted post
+                        var domain = "https://www.reddit.com";
+                        var mostUpvotedPost = filteredPosts
                             .OrderByDescending(p => p.data.ups)
                             .FirstOrDefault();
 
-                        var mostUpvotedPostUrl = mostUpvotedPost?.data.url.ToString();
-                        var mostUpvotedPostUpvotes = mostUpvotedPost?.data.ups ?? 0;
+                        var permalink = mostUpvotedPost?.data.permalink.ToString();
+                        var mostUpvotedPostUrl = domain + permalink;
+                        var mostUpvotedPostUpvotes = (int)mostUpvotedPost?.data.ups;
 
-                        // Explicitly cast jsonData.data.children to IEnumerable<dynamic>
-                        var userPostsCounts = ((IEnumerable<dynamic>)jsonData.data.children)
-                            .GroupBy(p => p.data.author.ToString())
-                            .Select(g => new
-                            {
-                                Username = g.Key,
-                                PostCount = g.Count()
-                            })
-                            .OrderByDescending(g => g.PostCount)
-                            .FirstOrDefault();
+                        //Extract the user with most post
+                        var userPostsCounts = filteredPosts
+    .Where(p => p.data.author != null)
+    .GroupBy(p => p.data.author.ToString())
+    .Select(g => new
+    {
+        Username = g.Key,
+        PostCount = g.Count()
+    })
+    .OrderByDescending(g => g.PostCount)
+    .FirstOrDefault();
+
+
 
                         var mostActiveUser = userPostsCounts?.Username;
-                        var mostActiveUserPostCount = userPostsCounts?.PostCount ?? 0;
+                        var mostActiveUserPostCount = userPostsCounts?.PostCount;
+
+                        //var mostActiveUser = "Apple";
+                        //var mostActiveUserPostCount =2;
 
                         // Return the extracted data
                         return Ok(new
                         {
-                            MostUpvotedPostUrl = mostUpvotedPostUrl ?? "No post after the start time",
+                            MostUpvotedPostUrl = mostUpvotedPostUrl,
                             MostUpvotedPostUpvotes = mostUpvotedPostUpvotes,
-                            MostActiveUser = mostActiveUser ?? "No users",
+                            MostActiveUser = mostActiveUser,
                             MostActiveUserPostCount = mostActiveUserPostCount
                         });
                     }
